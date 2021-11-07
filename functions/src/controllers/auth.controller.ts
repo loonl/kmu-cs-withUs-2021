8dde4679b9f884b45ada7258fe5425e77a94cbc2
SeiwonPark <psw7347@gmail.com>
2021-11-07 21:39:59 +0900

    chore: modified feature creating user

--- functions/src/controllers/auth.controller.ts
+++ functions/src/controllers/auth.controller.ts
@@ -4,23 +4,28 @@ import { admin } from "../config/firebase"
 
 /**
  * Create a new user
- * @route GET /user/create
+ * @route POST /user/create
  */
-export const createUser = (req: Request, res: Response): void => {
-  admin.auth().createUser({
-    email: "test@example.com",
-    emailVerified: false,
-    phoneNumber: "+11234567890",
-    password: "secretPassword",
-    displayName: "Test User",
-    photoURL: "http://www.example.com/12345678/photo.png",
-    disabled: false,
-  })
-    .then((userRecord) => {
-      console.log("Successfully created new user:", userRecord.uid)
-      res.status(200).send(userRecord.uid)
+export const createUser = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body
+    console.log(email, password)
+    if (!email && !password) {
+      console.log("No payload from user")
+      res.status(200).send({ "success": false }) // FIXME: throw empty payload error
+    }
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password,
     })
-    .catch((error) => {
-      console.log("Error creating new user:", error)
-    })
+    if (userRecord.uid) {
+      console.log(`Successfully created user ${userRecord.uid}`)
+      res.status(200).send({ "success": true })
+    } else {
+      console.log("Failed to create user")
+      res.status(200).send({ "success": false })
+    }
+  } catch (error) {
+    console.log("Error creating new user:", error)
+  }
 }
