d8f7013d426d1abd871d0ed21534ccad0d5cd311
yunsaerim <yun8601@naver.com>
2021-11-14 02:47:33 +0900

    로그인

--- app/src/main/java/com/with/us/LoginActivity.java
+++ app/src/main/java/com/with/us/LoginActivity.java
@@ -1,4 +1,28 @@
 package com.with.us;
 
-public class LoginActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        TextView register_tv  = (TextView) findViewById(R.id.activity_login_tv_register);
+        register_tv.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view){
+                Intent register_intent  = new Intent(LoginActivity.this, RegisterActivity.class);
+                LoginActivity.this.startActivity(register_intent);
+            }
+        });
+    }
 }
+
