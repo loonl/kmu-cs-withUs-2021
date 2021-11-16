131ae2758cab1aefa871fe0d954412702274b8bc
yunsaerim <yun8601@naver.com>
2021-11-16 18:52:08 +0900

    회원가입 프레그먼트 어뎁터 수정 , 회원정보 액티비티 추가

--- app/src/main/java/com/with/us/RegisterActivityFragment.java
+++ app/src/main/java/com/with/us/RegisterActivityFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class RegisterActivityFragment extends Fragment {
+
+    Spinner region_spinner,interest_spinner;
+    ArrayAdapter region_adapter,interest_adapter;
 
     public RegisterActivityFragment() {
 
@@ -19,7 +24,24 @@ public class RegisterActivityFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_register_fragment, container, false);
+        View register_view = inflater.inflate(R.layout.activity_register_fragment, container, false);
+
+        //지역스피너 Adapt
+        region_spinner = (Spinner) register_view.findViewById(R.id.activity_register_spn_region);
+        region_adapter = ArrayAdapter.createFromResource(getContext(), R.array.region, android.R.layout.simple_spinner_dropdown_item);
+        region_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        region_spinner.setAdapter(region_adapter);
+
+        //관심사스피너 Adapt
+        interest_spinner= (Spinner) register_view.findViewById(R.id.activity_register_spn_interest);
+        interest_adapter = ArrayAdapter.createFromResource(getContext(), R.array.interest, android.R.layout.simple_spinner_dropdown_item);
+        interest_adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        interest_spinner.setAdapter(interest_adapter);
+
+        return register_view;
     }
+
+
+
 
 }
