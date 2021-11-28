d502d2c7acc84494fb89ecb41bd7dea42c6dde00
tjtkdals0524 <tjtkdals0524@naver.com>
2021-11-29 00:46:04 +0900

    feat: added splash screen animation

--- app/src/main/java/com/with/us/InitActivity.java
+++ app/src/main/java/com/with/us/InitActivity.java
@@ -2,6 +2,10 @@ package com.with.us;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,17 +18,23 @@ public class InitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
         startService(new Intent(this, ServiceHandler.class));
+        TextView tv=findViewById(R.id.mainTitle);
+        ImageView iv=findViewById(R.id.mainLogo);
+
+        iv.startAnimation(AnimationUtils.loadAnimation(this,R.anim.bounce));
+        tv.startAnimation(AnimationUtils.loadAnimation(this,R.anim.linear));
 
         Thread thread = new Thread() {
             @Override
             public void run() {
                 try {
                     super.run();
-                    sleep(2000);
+                    sleep(3000);
                 } catch (Exception e) {
                 } finally {
                     Intent i = new Intent(InitActivity.this, MainActivity.class);
                     startActivity(i);
+                    overridePendingTransition(R.anim.decelerate_out,R.anim.decelerate_in);
                     finish();
                 }
             }
